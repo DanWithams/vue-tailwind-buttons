379d919ebd8a6c8bffab2236bfb1f24f289bf483
Daniel Withams <me@danwithams.com>
2024-03-18 16:07:48 +0000

    Another attempt

--- buttons/index.js
+++ buttons/index.js
@@ -5,7 +5,7 @@ import {mergeTwClasses} from "tailwind-helpers";
 let baseClasses = ['px-6', 'py-2', 'font-semibold'];
 let baseInsetClasses = [];
 let baseOuterClasses = [];
-let buttonRoundedDefault = ref('rounded');
+export const buttonRoundedDefault = ref('rounded');
 
 export function getButtonRoundedDefault() {
     console.log('getButtonRoundedDefault', buttonRoundedDefault.value);
