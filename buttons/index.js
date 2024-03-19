39829ff690fdfd960451606a3fdc09870efaeb74
Daniel Withams <me@danwithams.com>
2024-03-19 11:43:39 +0000

    Fixing defaults

--- buttons/index.js
+++ buttons/index.js
@@ -1,6 +1,5 @@
 import {computed, ref} from "vue";
 import {mergeTwClasses} from "tailwind-helpers";
-import {buttonRoundedDefault} from "./defaults";
 
 let baseClasses = ['px-6', 'py-2', 'font-semibold'];
 let baseInsetClasses = [];
