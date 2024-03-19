b1e2a0c3edcaf26a2c7b2890b939b04bd803a0ad
Daniel Withams <me@danwithams.com>
2024-03-19 10:25:31 +0000

    MOre debug

--- buttons/index.js
+++ buttons/index.js
@@ -39,6 +39,7 @@ export function useBaseButton() {
 export function useButtons(props, options = { classes: [], classesOutline: [], insetClasses: [], insetClassesOutline: [] }) {
     const buttonClasses = computed(() => {
         console.log(buttonRoundedDefault.value);
+        console.log(getButtonRoundedDefault());
         const classes = ! props.outline
             ? mergeTwClasses(options.classes, props.rounded || buttonRoundedDefault.value)
             : mergeTwClasses(options.classesOutline, props.rounded || buttonRoundedDefault.value);
@@ -52,6 +53,7 @@ export function useButtons(props, options = { classes: [], classesOutline: [], i
 
     const buttonInsetClasses = computed(() => {
         console.log(buttonRoundedDefault.value);
+        console.log(getButtonRoundedDefault());
         const classes = ! props.outline
             ? mergeTwClasses(options.insetClasses, props.rounded || buttonRoundedDefault.value)
             : mergeTwClasses(options.insetClassesOutline, props.rounded || buttonRoundedDefault.value);
