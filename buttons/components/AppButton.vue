f017c8e2c3222856b66673f9ce5a78f0970769ce
Daniel Withams <me@danwithams.com>
2024-03-19 15:06:53 +0000

    Adding disabled

--- buttons/components/AppButton.vue
+++ buttons/components/AppButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <button type="button" class="focus:outline-none group" :class="classes">
+    <button type="button" class="focus:outline-none group" :class="classes" disabled="disabled">
         <div class="relative select-none" :class="baseClasses">
             <div class="absolute inset-0" :class="insetClasses"></div>
             <div class="flex flex-row justify-center items-center">
@@ -26,13 +26,20 @@ const props = defineProps({
         type: Boolean,
         required: false,
         default: false,
+    },
+    disabled: {
+        type: Boolean,
+        required: false,
+        default: false,
     }
 });
 
 const classes = computed(() => {
-    return props.outline
-        ? _.get(defaults, props.variant + '.outline.classes')
-        : _.get(defaults, props.variant + '.solid.classes')
+    return (
+            props.outline
+                ? _.get(defaults, props.variant + '.outline.classes')
+                : _.get(defaults, props.variant + '.solid.classes')
+        ).concat(['cursor-not-allowed']);
 });
 
 const baseClasses = computed(() => {
