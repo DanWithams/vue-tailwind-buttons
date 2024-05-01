2cd52b6b624a30a35858ceedac9ade22ea051f44
Daniel Withams <me@danwithams.com>
2024-05-01 15:29:32 +0100

    Adding __dirname

--- plugins/vite-config-plugin.js
+++ plugins/vite-config-plugin.js
@@ -1,5 +1,8 @@
-import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
 import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Async function to load config
 async function loadConfig(configPath) {
