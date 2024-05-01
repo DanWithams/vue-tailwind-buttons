bb8a7d6d520928ba17badcf11dbc926b6092ad46
Daniel Withams <me@danwithams.com>
2024-05-01 13:03:00 +0100

    Revert

--- plugins/vite-config-plugin.js
+++ plugins/vite-config-plugin.js
@@ -1,26 +1,21 @@
-const fs = require('fs');
-const path = require('path');
+import { readFileSync } from 'fs';
+import path from 'path';
 
 export default function configPlugin() {
     return {
         name: 'vue-tailwind-buttons-vite-plugin', // required, will show up in warnings and errors
         enforce: 'pre',        // run before other plugins
         config() {
-            try {
-                // Read and parse the configuration file
-                const configPath = path.resolve(__dirname, 'vue-tailwind-button.config.json');
-                const configData = JSON.parse(fs.readFileSync(configPath, 'utf-8'));
+            // Read and parse the configuration file
+            const configPath = path.resolve(__dirname, 'vue-tailwind-button.config.json');
+            const configData = JSON.parse(readFileSync(configPath, 'utf-8'));
 
-                // Return a modified configuration object
-                return {
-                    define: {
-                        __EXTERNAL_CONFIG__: JSON.stringify(configData || {}),
-                    },
-                };
-            } catch (e) {
-                console.error('Failed to load configuration:', e);
-                throw e;  // Stop the build if configuration cannot be loaded
-            }
+            // Return a modified configuration object
+            return {
+                define: {
+                    __EXTERNAL_CONFIG__: JSON.stringify(configData || {}),
+                },
+            };
         },
     };
 }
