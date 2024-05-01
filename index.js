11d7c5de93db0f952816c2e529e7cea2744842ce
Daniel Withams <me@danwithams.com>
2024-05-01 17:26:20 +0100

    Updating all exports

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
+import ButtonDefault from "./src/components/ButtonDefault.vue";
 import ButtonPrimary from "./src/components/ButtonPrimary.vue";
+import ButtonSecondary from "./src/components/ButtonSecondary.vue";
+import ButtonPositive from "./src/components/ButtonPositive.vue";
+import ButtonNegative from "./src/components/ButtonNegative.vue";
+import ButtonTransparent from "./src/components/ButtonTransparent.vue";
+
+
 
 export default {
-    ButtonPrimary
+    ButtonDefault,
+    ButtonPrimary,
+    ButtonSecondary,
+    ButtonPositive,
+    ButtonNegative,
+    ButtonTransparent,
 }
 
+export { default as ButtonDefault } from "./src/components/ButtonDefault.vue";
 export { default as ButtonPrimary } from "./src/components/ButtonPrimary.vue";
+export { default as ButtonSecondary } from "./src/components/ButtonSecondary.vue";
+export { default as ButtonPositive } from "./src/components/ButtonPositive.vue";
+export { default as ButtonNegative } from "./src/components/ButtonNegative.vue";
+export { default as ButtonTransparent } from "./src/components/ButtonTransparent.vue";
