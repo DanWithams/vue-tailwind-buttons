658eb7cecb7ac429b44624502b491271a1b175ab
Daniel Withams <me@danwithams.com>
2024-05-01 13:04:25 +0100

    Fixing

--- plugins/vite-config-plugin.js
+++ plugins/vite-config-plugin.js
@@ -1,5 +1,10 @@
 import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
 import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 export default function configPlugin() {
     return {
