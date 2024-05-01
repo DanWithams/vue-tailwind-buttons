952596ae8738206753124d95d306caaaa62dea84
Daniel Withams <me@danwithams.com>
2024-05-01 17:21:34 +0100

    Fixing plugin

--- plugins/vite-config-plugin.js
+++ plugins/vite-config-plugin.js
@@ -1,22 +1,22 @@
-import { fileURLToPath } from 'url';
 import path from 'path';
 
-// Async function to load config
-async function loadConfig(configPath) {
-    // delete require.cache[require.resolve(configPath)];
-    try {
-        const configModule = await import(configPath + '?update=' + new Date().getTime());
-        return configModule.default || {};
-    } catch (error) {
-        return {};
-    }
-}
-
 export default function configPlugin(options = {}) {
+
+    // Async function to load config
+    async function loadConfig(configPath) {
+        // delete require.cache[require.resolve(configPath)];
+        try {
+            const configModule = await import(configPath + '?update=' + new Date().getTime());
+            return configModule.default || {};
+        } catch (error) {
+            return {};
+        }
+    }
+
     const configPath = path.resolve(process.cwd(), 'vue-tailwind-button.config.js');
+
     return {
         name: 'vue-tailwind-buttons-vite-plugin', // required, will show up in warnings and errors
-        enforce: 'post',        // run before other plugins
         async config() {
             // Return a modified configuration object
             const configData = await loadConfig(configPath);
