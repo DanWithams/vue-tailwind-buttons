df745de61ba4fc2c103db88a48f101448460baa4
Daniel Withams <me@danwithams.com>
2024-05-01 14:41:50 +0100

    Testing

--- plugins/vite-config-plugin.js
+++ plugins/vite-config-plugin.js
@@ -38,8 +38,28 @@ export default function configPlugin(options = {}) {
                 configData = await loadConfig(configPath);
                 // Update the define values
                 console.log(JSON.stringify(configData || {}));
+                // server.config.define = {
+                //     __VUE_TAILWIND_BUTTONS_EXTERNAL_CONFIG__: JSON.stringify(configData || {}),
+                // };
                 server.config.define = {
-                    __VUE_TAILWIND_BUTTONS_EXTERNAL_CONFIG__: JSON.stringify(configData || {}),
+                    __VUE_TAILWIND_BUTTONS_EXTERNAL_CONFIG__: JSON.stringify({
+                        "primary": {
+                            "solid": {
+                                "classes": [
+                                    "bg-red-400",
+                                    "text-zinc-100",
+                                    "dark:bg-red-500",
+                                    "hover:bg-indigo-500",
+                                    "dark:hover:bg-indigo-700",
+                                    "focus:bg-indigo-500",
+                                    "dark:focus:bg-indigo-700"
+                                ],
+                                "insetClasses": [
+                                    "border-0"
+                                ]
+                            }
+                        }
+                    }),
                 };
                 // Trigger a full reload
                 server.moduleGraph.invalidateAll(); // Invalidate the entire module graph to force a re-import
